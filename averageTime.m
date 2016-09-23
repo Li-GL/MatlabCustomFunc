39504886c1a9fe9293a5587b8f9846aa806b16b7
lglfa <lglfa666@gmail.com>
2016-09-23 12:24:04 +0800

    add the  removeTime

--- averageTime.m
+++ averageTime.m
@@ -1,23 +1,31 @@
-function [outDatetime outputData] = averageTime(inputDateime, inputData,timeInterval, startTime )
-%[dateTime outputData] = averagetime(timeInterval, startTime, inputData)
-%Note that inputDateime should be date or time Number. 
-% Unit of timeInterval is min
-% startTime is date Number, usually equals to inputDatetime(1)
+function [outDatetime outputData] = averageTime(inputDatetime, inputData,startTime, timeInterval, removeTime)
+%[outDatetime outputData] = averageTime(inputDateime, inputData,timeInterval, startTime )
+%Note that inputDateime should be dateNumber. 
+% startTime is when you start the experiment, it's date Number, usually equals to inputDatetime(1)
+% timeInterval is the interval you want to do averaging, removeTime is time
+% you want to remove in both front and end of your dataset
+%Unit of timeInterval and removeTime is min
 % do averaging every timeInterval time
-% 14:30?????????14:00-14:30 
+% 14:30代表的是14:00-14:30
+
+%%譬如想平均14:00 - 14:15的数据，但是14:00-14:02和14:13-14:15的数据不参与平均，则输入应该为
+%averageTime(inputDatetime, inputData,14:00, 15, 2)
+%注意这里假设开始时间inputDatetime(1) = 14:00
 
 
 timeinterval_num = timeInterval/60/24;
+removeTime = removeTime/60/24;
 
-%?????????????end
 
 time=startTime;
 
-%?????index??????????nanmean
+%找index，消除NAN求平均nanmean
 i=1;
-while time <= inputDateime(end)
+while time <= inputDatetime(end)
     
-    ind = find(inputDateime>=(startTime+(i-1)*timeinterval_num) & inputDateime<=(startTime+i*timeinterval_num));
+    
+    
+    ind = find(inputDatetime>= time + removeTime & inputDatetime<=(time+i*timeinterval_num-removeTime));
     outDatetime(i) = startTime+i*timeinterval_num;
     outputData(i) = nanmean(inputData(ind));
     
